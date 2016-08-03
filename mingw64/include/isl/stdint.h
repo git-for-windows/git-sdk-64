c5210b365a6794c2bab13a6b40a82f750e05307b
Johannes Schindelin <johannes.schindelin@gmx.de>
2016-08-03 14:01:35 +0200

    mingw-w64-x86_64-isl-0.16.1-1
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

---
+++ mingw64/include/isl/stdint.h
@@ -0,0 +1,9 @@
+#ifndef _ISL_INCLUDE_ISL_STDINT_H
+#define _ISL_INCLUDE_ISL_STDINT_H 1
+#ifndef _GENERATED_STDINT_H
+#define _GENERATED_STDINT_H "isl 0.16.1"
+/* generated using gnu compiler gcc.exe (Rev2, Built by MSYS2 project) 5.3.0 */
+#define _STDINT_HAVE_STDINT_H 1
+#include <stdint.h>
+#endif
+#endif
