0f44d377e63abd4b38dc8b89febc1a8fc19b9b37
Johannes Schindelin <johannes.schindelin@gmx.de>
2016-08-03 13:56:35 +0200

    pkgfile-15-1
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

---
+++ usr/share/doc/pkgfile/command-not-found.bash
@@ -0,0 +1,16 @@
+command_not_found_handle () {
+  local pkgs cmd=$1
+  local FUNCNEST=10
+
+  set +o verbose
+
+  mapfile -t pkgs < <(pkgfile -bv -- "$cmd" 2>/dev/null)
+  if (( ${#pkgs[*]} )); then
+    printf '%s may be found in the following packages:\n' "$cmd"
+    printf '  %s\n' "${pkgs[@]}"
+    return 0
+  else
+    printf "bash: %s: command not found\n" "$cmd" >&2
+    return 127
+  fi
+}
---
+++ usr/share/doc/pkgfile/command-not-found.zsh
@@ -0,0 +1,12 @@
+command_not_found_handler() {
+  local pkgs cmd="$1"
+
+  pkgs=(${(f)"$(pkgfile -b -v -- "$cmd" 2>/dev/null)"})
+  if [[ -n "$pkgs" ]]; then
+    printf '%s may be found in the following packages:\n' "$cmd"
+    printf '  %s\n' $pkgs[@]
+    return 0
+  fi
+
+  return 127
+}
