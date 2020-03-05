4ff214f71d7a0137d44aab13fd015e7003cef7a7
Git for Windows Build Agent <ci@git-for-windows.build>
2020-03-05 03:36:05 +0000

    Update 20200305-033601
    Signed-off-by: Git for Windows Build Agent <ci@git-for-windows.build>

---
+++ mingw32/lib/sqlite3.31.1/pkgIndex.tcl
@@ -0,0 +1,1 @@
+package ifneeded sqlite3 3.31.1 [list load [file join $dir libtclsqlite3[info sharedlibextension]] sqlite3]
--- mingw64/include/isl/stdint.h
+++ mingw64/include/isl/stdint.h
@@ -1,7 +1,7 @@
 #ifndef _ISL_INCLUDE_ISL_STDINT_H
 #define _ISL_INCLUDE_ISL_STDINT_H 1
 #ifndef _GENERATED_STDINT_H
-#define _GENERATED_STDINT_H "isl 0.22"
+#define _GENERATED_STDINT_H "isl 0.22.1"
 /* generated using gnu compiler gcc.exe (Rev2, Built by MSYS2 project) 9.2.0 */
 #define _STDINT_HAVE_STDINT_H 1
 #include <stdint.h>
