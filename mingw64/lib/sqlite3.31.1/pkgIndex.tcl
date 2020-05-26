06ff114188d6c2695ccb15c4327e80075d4a5ba9
Git for Windows Build Agent <ci@git-for-windows.build>
2020-05-26 03:08:36 +0000

    Update 20200526-030832
    Signed-off-by: Git for Windows Build Agent <ci@git-for-windows.build>

--- mingw64/lib/sqlite3.31.1/pkgIndex.tcl
+++
@@ -1,1 +0,0 @@
-package ifneeded sqlite3 3.31.1 [list load [file join $dir libtclsqlite3[info sharedlibextension]] sqlite3]
--- mingw64/share/glib-2.0/codegen/config.py
+++ mingw64/share/glib-2.0/codegen/config.py
@@ -19,6 +19,6 @@
 #
 # Author: David Zeuthen <davidz@redhat.com>
 
-VERSION = "2.64.2"
+VERSION = "2.64.3"
 MAJOR_VERSION = 2
 MINOR_VERSION = 64
