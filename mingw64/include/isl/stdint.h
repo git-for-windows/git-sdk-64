c58b3a7feffc68479d2315e79bef633a14fa139c
Git for Windows Build Agent <ci@git-for-windows.build>
2020-10-11 03:12:51 +0000

    Update 20 packages
    mingw-w64-i686-gcc (10.2.0-3 -> 10.2.0-4)
    mingw-w64-i686-gcc-fortran (10.2.0-3 -> 10.2.0-4)
    mingw-w64-i686-gcc-libgfortran (10.2.0-3 -> 10.2.0-4)
    mingw-w64-i686-gcc-libs (10.2.0-3 -> 10.2.0-4)
    mingw-w64-i686-gmp (6.2.0-1 -> 6.2.0-2)
    mingw-w64-i686-gnutls (3.6.15-1 -> 3.6.15-2)
    mingw-w64-i686-isl (0.22.1-1 -> 0.22.1-2)
    mingw-w64-i686-mpc (1.2.0-1 -> 1.2.0-2)
    mingw-w64-i686-mpfr (4.1.0-2 -> 4.1.0-3)
    mingw-w64-i686-nettle (3.6-1 -> 3.6-2)
    mingw-w64-i686-rtmpdump-git-r514.c5f04a5 (2 -> 3)
    mingw-w64-x86_64-gcc (10.2.0-3 -> 10.2.0-4)
    mingw-w64-x86_64-gcc-libs (10.2.0-3 -> 10.2.0-4)
    mingw-w64-x86_64-gmp (6.2.0-1 -> 6.2.0-2)
    mingw-w64-x86_64-gnutls (3.6.15-1 -> 3.6.15-2)
    mingw-w64-x86_64-isl (0.22.1-1 -> 0.22.1-2)
    mingw-w64-x86_64-mpc (1.2.0-1 -> 1.2.0-2)
    mingw-w64-x86_64-mpfr (4.1.0-2 -> 4.1.0-3)
    mingw-w64-x86_64-nettle (3.6-1 -> 3.6-2)
    mingw-w64-x86_64-rtmpdump-git-r514.c5f04a5 (2 -> 3)
    
    Signed-off-by: Git for Windows Build Agent <ci@git-for-windows.build>

--- mingw64/include/isl/stdint.h
+++ mingw64/include/isl/stdint.h
@@ -2,7 +2,7 @@
 #define _ISL_INCLUDE_ISL_STDINT_H 1
 #ifndef _GENERATED_STDINT_H
 #define _GENERATED_STDINT_H "isl 0.22.1"
-/* generated using gnu compiler gcc.exe (Rev2, Built by MSYS2 project) 9.2.0 */
+/* generated using gnu compiler gcc.exe (Rev3, Built by MSYS2 project) 10.2.0 */
 #define _STDINT_HAVE_STDINT_H 1
 #include <stdint.h>
 #endif
