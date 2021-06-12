7280cd5fef6a0e09058e10c0ed5861af6d2fd898
Git for Windows Build Agent <ci@git-for-windows.build>
2021-06-12 03:12:37 +0000

    Update 2 packages
    pacman (5.2.2-23 -> 6.0.0-2)
    pacman-contrib (1.4.0-1 -> 1.4.0-2)
    
    Signed-off-by: Git for Windows Build Agent <ci@git-for-windows.build>

--- usr/share/makepkg/buildenv.sh
+++ usr/share/makepkg/buildenv.sh
@@ -3,7 +3,7 @@
 #   buildenv.sh - functions for altering the build environment before
 #   compilation
 #
-#   Copyright (c) 2015-2020 Pacman Development Team <pacman-dev@archlinux.org>
+#   Copyright (c) 2015-2021 Pacman Development Team <pacman-dev@archlinux.org>
 #
 #   This program is free software; you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
