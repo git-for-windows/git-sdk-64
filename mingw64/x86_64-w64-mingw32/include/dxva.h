4e610ca77e733b12ce1f974a18ba267f1e27d86f
Git for Windows Build Agent <ci@git-for-windows.build>
2021-08-03 03:11:21 +0000

    Update 12 packages
    mingw-w64-i686-crt-git (9.0.0.6246.ae63cde27-1 -> 9.0.0.6270.0e0eed623-1)
    mingw-w64-i686-headers-git (9.0.0.6246.ae63cde27-1 -> 9.0.0.6270.0e0eed623-1)
    mingw-w64-i686-libmangle-git (9.0.0.6246.ae63cde27-1 -> 9.0.0.6270.0e0eed623-1)
    mingw-w64-i686-libwinpthread-git (9.0.0.6246.ae63cde27-1 -> 9.0.0.6270.0e0eed623-1)
    mingw-w64-i686-tools-git (9.0.0.6246.ae63cde27-1 -> 9.0.0.6270.0e0eed623-1)
    mingw-w64-i686-winpthreads-git (9.0.0.6246.ae63cde27-1 -> 9.0.0.6270.0e0eed623-1)
    mingw-w64-x86_64-crt-git (9.0.0.6246.ae63cde27-1 -> 9.0.0.6270.0e0eed623-1)
    mingw-w64-x86_64-headers-git (9.0.0.6246.ae63cde27-1 -> 9.0.0.6270.0e0eed623-1)
    mingw-w64-x86_64-libmangle-git (9.0.0.6246.ae63cde27-1 -> 9.0.0.6270.0e0eed623-1)
    mingw-w64-x86_64-libwinpthread-git (9.0.0.6246.ae63cde27-1 -> 9.0.0.6270.0e0eed623-1)
    mingw-w64-x86_64-tools-git (9.0.0.6246.ae63cde27-1 -> 9.0.0.6270.0e0eed623-1)
    mingw-w64-x86_64-winpthreads-git (9.0.0.6246.ae63cde27-1 -> 9.0.0.6270.0e0eed623-1)
    
    Signed-off-by: Git for Windows Build Agent <ci@git-for-windows.build>

--- mingw64/x86_64-w64-mingw32/include/dxva.h
+++ mingw64/x86_64-w64-mingw32/include/dxva.h
@@ -578,18 +578,17 @@ typedef struct _DXVA_PicEntry_AV1 {
     UINT width;
     UINT height;
 
-    // Global motion parameters
     INT wmmat[6];
-    union {
-        struct {
+    __C89_NAMELESS union {
+        __C89_NAMELESS struct {
             UCHAR wminvalid : 1;
             UCHAR wmtype : 2;
             UCHAR Reserved : 5;
-        };
+        } __C89_NAMELESSSTRUCTNAME;
         UCHAR GlobalMotionFlags;
-    };
+    } __C89_NAMELESSUNIONNAME;
     UCHAR Index;
-    USHORT Reserved16Bits;
+    UINT16 Reserved16Bits;
 } DXVA_PicEntry_AV1, *LPDXVA_PicEntry_AV1;
 
 /* AV1 picture parameters data structure */
@@ -605,7 +604,6 @@ typedef struct _DXVA_PicParams_AV1 {
     UCHAR bitdepth;
     UCHAR seq_profile;
 
-    // Tiles:
     struct {
         UCHAR cols;
         UCHAR rows;
@@ -614,9 +612,8 @@ typedef struct _DXVA_PicParams_AV1 {
         USHORT heights[64];
     } tiles;
 
-    // Coding Tools
     union {
-        struct {
+        __C89_NAMELESS struct {
             UINT use_128x128_superblock : 1;
             UINT intra_edge_filter : 1;
             UINT interintra_compound : 1;
@@ -644,13 +641,12 @@ typedef struct _DXVA_PicParams_AV1 {
             UINT enable_ref_frame_mvs : 1;
             UINT reference_frame_update : 1;
             UINT Reserved : 5;
-        };
+        } __C89_NAMELESSSTRUCTNAME;
         UINT32 CodingParamToolFlags;
     } coding;
 
-    // Format & Picture Info flags
     union {
-        struct {
+        __C89_NAMELESS struct {
             UCHAR frame_type : 2;
             UCHAR show_frame : 1;
             UCHAR showable_frame : 1;
@@ -658,11 +654,10 @@ typedef struct _DXVA_PicParams_AV1 {
             UCHAR subsampling_y : 1;
             UCHAR mono_chrome : 1;
             UCHAR Reserved : 1;
-        };
+        } __C89_NAMELESSSTRUCTNAME;
         UCHAR FormatAndPictureInfoFlags;
     } format;
 
-    // References
     UCHAR primary_ref_frame;
     UCHAR order_hint;
     UCHAR order_hint_bits;
@@ -670,23 +665,22 @@ typedef struct _DXVA_PicParams_AV1 {
     DXVA_PicEntry_AV1 frame_refs[7];
     UCHAR RefFrameMapTextureIndex[8];
 
-    // Loop filter parameters
     struct {
         UCHAR filter_level[2];
         UCHAR filter_level_u;
         UCHAR filter_level_v;
 
         UCHAR sharpness_level;
-        union {
-            struct {
+        __C89_NAMELESS union {
+            __C89_NAMELESS struct {
                 UCHAR mode_ref_delta_enabled : 1;
                 UCHAR mode_ref_delta_update : 1;
                 UCHAR delta_lf_multi : 1;
                 UCHAR delta_lf_present : 1;
                 UCHAR Reserved : 4;
-            };
+            } __C89_NAMELESSSTRUCTNAME;
             UCHAR ControlFlags;
-        } DUMMYUNIONNAME;
+        } __C89_NAMELESSUNIONNAME;
         CHAR ref_deltas[8];
         CHAR mode_deltas[2];
         UCHAR delta_lf_res;
@@ -695,16 +689,15 @@ typedef struct _DXVA_PicParams_AV1 {
         UINT16 Reserved16Bits;
     } loop_filter;
 
-    // Quantization
     struct {
-        union {
-            struct {
+        __C89_NAMELESS union {
+            __C89_NAMELESS struct {
                 UCHAR delta_q_present : 1;
                 UCHAR delta_q_res : 2;
                 UCHAR Reserved : 5;
-            };
+            } __C89_NAMELESSSTRUCTNAME;
             UCHAR ControlFlags;
-        } DUMMYUNIONNAME;
+        } __C89_NAMELESSUNIONNAME;
 
         UCHAR base_qindex;
         CHAR y_dc_delta_q;
@@ -712,37 +705,35 @@ typedef struct _DXVA_PicParams_AV1 {
         CHAR v_dc_delta_q;
         CHAR u_ac_delta_q;
         CHAR v_ac_delta_q;
-        // using_qmatrix:
         UCHAR qm_y;
         UCHAR qm_u;
         UCHAR qm_v;
         UINT16 Reserved16Bits;
     } quantization;
 
-    // Cdef parameters
     struct {
-        union {
-            struct {
+        __C89_NAMELESS union {
+            __C89_NAMELESS struct {
                 UCHAR damping : 2;
                 UCHAR bits : 2;
                 UCHAR Reserved : 4;
-            };
+            } __C89_NAMELESSSTRUCTNAME;
             UCHAR ControlFlags;
-        } DUMMYUNIONNAME;
+        } __C89_NAMELESSUNIONNAME;
 
         union {
-            struct {
+            __C89_NAMELESS struct {
                 UCHAR primary : 6;
                 UCHAR secondary : 2;
-            };
+            } __C89_NAMELESSSTRUCTNAME;
             UCHAR combined;
         } y_strengths[8];
 
         union {
-            struct {
+            __C89_NAMELESS struct {
                 UCHAR primary : 6;
                 UCHAR secondary : 2;
-            };
+            } __C89_NAMELESSSTRUCTNAME;
             UCHAR combined;
         } uv_strengths[8];
 
@@ -750,22 +741,21 @@ typedef struct _DXVA_PicParams_AV1 {
 
     UCHAR interp_filter;
 
-    // Segmentation
     struct {
-        union {
-            struct {
+        __C89_NAMELESS union {
+            __C89_NAMELESS struct {
                 UCHAR enabled : 1;
                 UCHAR update_map : 1;
                 UCHAR update_data : 1;
                 UCHAR temporal_update : 1;
                 UCHAR Reserved : 4;
-            };
+            } __C89_NAMELESSSTRUCTNAME;
             UCHAR ControlFlags;
-        } DUMMYUNIONNAME;
+        } __C89_NAMELESSUNIONNAME;
         UCHAR Reserved24Bits[3];
 
         union {
-            struct {
+            __C89_NAMELESS struct {
                 UCHAR alt_q : 1;
                 UCHAR alt_lf_y_v : 1;
                 UCHAR alt_lf_y_h : 1;
@@ -774,7 +764,7 @@ typedef struct _DXVA_PicParams_AV1 {
                 UCHAR ref_frame : 1;
                 UCHAR skip : 1;
                 UCHAR globalmv : 1;
-            };
+            } __C89_NAMELESSSTRUCTNAME;
             UCHAR mask;
         } feature_mask[8];
 
@@ -783,8 +773,8 @@ typedef struct _DXVA_PicParams_AV1 {
     } segmentation;
 
     struct {
-        union {
-            struct {
+        __C89_NAMELESS union {
+            __C89_NAMELESS struct {
                 USHORT apply_grain : 1;
                 USHORT scaling_shift_minus8 : 2;
                 USHORT chroma_scaling_from_luma : 1;
@@ -795,9 +785,9 @@ typedef struct _DXVA_PicParams_AV1 {
                 USHORT clip_to_restricted_range : 1;
                 USHORT matrix_coeff_is_identity : 1;
                 USHORT Reserved : 3;
-            };
+            } __C89_NAMELESSSTRUCTNAME;
             USHORT ControlFlags;
-        } DUMMYUNIONNAME;
+        } __C89_NAMELESSUNIONNAME;
 
         USHORT grain_seed;
         UCHAR scaling_points_y[14][2];
@@ -828,18 +818,18 @@ typedef struct _DXVA_Tile_AV1 {
     UINT   DataSize;
     USHORT row;
     USHORT column;
-    USHORT Reserved16Bits;
+    UINT16 Reserved16Bits;
     UCHAR  anchor_frame;
-    UCHAR Reserved8Bits;
+    UCHAR  Reserved8Bits;
 } DXVA_Tile_AV1, *LPDXVA_Tile_AV1;
 
 typedef struct _DXVA_Status_AV1 {
     UINT  StatusReportFeedbackNumber;
     DXVA_PicEntry_AV1 CurrPic;
-    UCHAR  bBufType;
-    UCHAR  bStatus;
-    UCHAR  bReserved8Bits;
-    USHORT wNumMbsAffected;
+    UCHAR  BufType;
+    UCHAR  Status;
+    UCHAR  Reserved8Bits;
+    USHORT NumMbsAffected;
 } DXVA_Status_AV1, *LPDXVA_Status_AV1;
 
 #include <poppack.h>
