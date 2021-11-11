a8b648c4b4b9a982803f791523c82149d4a44009
Git for Windows Build Agent <ci@git-for-windows.build>
2021-11-11 03:12:11 +0000

    Update 10 packages
    mingw-w64-i686-gcc (11.2.0-1 -> 11.2.0-2)
    mingw-w64-i686-gcc-fortran (11.2.0-1 -> 11.2.0-2)
    mingw-w64-i686-gcc-libgfortran (11.2.0-1 -> 11.2.0-2)
    mingw-w64-i686-gcc-libs (11.2.0-1 -> 11.2.0-2)
    mingw-w64-i686-python (3.9.7-2 -> 3.9.7-3)
    mingw-w64-x86_64-gcc (11.2.0-1 -> 11.2.0-2)
    mingw-w64-x86_64-gcc-libs (11.2.0-1 -> 11.2.0-2)
    mingw-w64-x86_64-git (2.34.0.rc1.windows.1-1 -> 2.34.0.rc2.windows.1-1)
    mingw-w64-x86_64-git-doc-html (2.34.0.rc1.windows.1-1 -> 2.34.0.rc2.windows.1-1)
    mingw-w64-x86_64-python (3.9.7-2 -> 3.9.7-3)
    
    Signed-off-by: Git for Windows Build Agent <ci@git-for-windows.build>

--- mingw64/lib/python3.9/distutils/command/build_ext.py
+++ mingw64/lib/python3.9/distutils/command/build_ext.py
@@ -219,7 +219,7 @@ class build_ext(Command):
         # For extensions under Cygwin, Python's library directory must be
         # appended to library_dirs
         if sys.platform[:6] == 'cygwin' or self.plat_name.startswith(('mingw')):
-            if sys.executable.startswith(os.path.join(sys.exec_prefix, "bin")):
+            if not sysconfig.python_build:
                 # building third party extensions
                 config_dir_name = os.path.basename(sysconfig.get_config_var('LIBPL'))
                 self.library_dirs.append(os.path.join(sys.prefix, "lib",
