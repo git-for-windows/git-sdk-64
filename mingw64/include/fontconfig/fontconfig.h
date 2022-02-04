3673a53f5b655bdd13b79aabe4106f8646aa4536
Git for Windows Build Agent <ci@git-for-windows.build>
2022-02-04 03:02:20 +0000

    Update 3 packages
    filesystem (2021.11-2 -> 2022.01-2)
    mingw-w64-x86_64-fontconfig (2.13.94-1 -> 2.13.95-1)
    mingw-w64-x86_64-glib2 (2.70.2-2 -> 2.70.3-1)
    
    Signed-off-by: Git for Windows Build Agent <ci@git-for-windows.build>

--- mingw64/include/fontconfig/fontconfig.h
+++ mingw64/include/fontconfig/fontconfig.h
@@ -53,7 +53,7 @@ typedef int		FcBool;
 
 #define FC_MAJOR	2
 #define FC_MINOR	13
-#define FC_REVISION	94
+#define FC_REVISION	95
 
 #define FC_VERSION	((FC_MAJOR * 10000) + (FC_MINOR * 100) + (FC_REVISION))
 
@@ -67,7 +67,7 @@ typedef int		FcBool;
  * it means multiple copies of the font information.
  */
 
-#define FC_CACHE_VERSION_NUMBER	7
+#define FC_CACHE_VERSION_NUMBER	8
 #define _FC_STRINGIFY_(s)    	#s
 #define _FC_STRINGIFY(s)    	_FC_STRINGIFY_(s)
 #define FC_CACHE_VERSION    	_FC_STRINGIFY(FC_CACHE_VERSION_NUMBER)
--- mingw64/share/glib-2.0/codegen/config.py
+++ mingw64/share/glib-2.0/codegen/config.py
@@ -19,6 +19,6 @@
 #
 # Author: David Zeuthen <davidz@redhat.com>
 
-VERSION = "2.70.2"
+VERSION = "2.70.3"
 MAJOR_VERSION = 2
 MINOR_VERSION = 70
