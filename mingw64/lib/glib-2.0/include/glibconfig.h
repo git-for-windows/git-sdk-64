e98c5849f85bb8dfb00ac589b9457e11c0f48fe4
Git for Windows Build Agent <ci@git-for-windows.build>
2022-04-16 03:20:59 +0000

    Update 1 package
    mingw-w64-x86_64-glib2 (2.72.0-2 -> 2.72.1-1)
    
    Signed-off-by: Git for Windows Build Agent <ci@git-for-windows.build>

--- mingw64/lib/glib-2.0/include/glibconfig.h
+++ mingw64/lib/glib-2.0/include/glibconfig.h
@@ -112,7 +112,7 @@ typedef unsigned long long guintptr;
 
 #define GLIB_MAJOR_VERSION 2
 #define GLIB_MINOR_VERSION 72
-#define GLIB_MICRO_VERSION 0
+#define GLIB_MICRO_VERSION 1
 
 #define G_OS_WIN32
 #define G_PLATFORM_WIN32
--- mingw64/share/glib-2.0/codegen/config.py
+++ mingw64/share/glib-2.0/codegen/config.py
@@ -19,6 +19,6 @@
 #
 # Author: David Zeuthen <davidz@redhat.com>
 
-VERSION = "2.72.0"
+VERSION = "2.72.1"
 MAJOR_VERSION = 2
 MINOR_VERSION = 72
