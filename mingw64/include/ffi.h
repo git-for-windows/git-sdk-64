7114b5e7850f2fdd5f66866d2e6b74008e99a18c
Git for Windows Build Agent <ci@git-for-windows.build>
2022-10-30 04:06:14 +0000

    Update 5 packages
    mingw-w64-i686-libffi (3.4.3-1 -> 3.4.4-1)
    mingw-w64-x86_64-antiword (0.37-2 -> 0.37-3)
    mingw-w64-x86_64-connect (1.105-2 -> 1.105-3)
    mingw-w64-x86_64-libffi (3.4.3-1 -> 3.4.4-1)
    mingw-w64-x86_64-odt2txt (0.5-2 -> 0.5-3)
    
    Signed-off-by: Git for Windows Build Agent <ci@git-for-windows.build>

--- mingw64/include/ffi.h
+++ mingw64/include/ffi.h
@@ -1,5 +1,5 @@
 /* -----------------------------------------------------------------*-C-*-
-   libffi 3.4.3
+   libffi 3.4.4
      - Copyright (c) 2011, 2014, 2019, 2021, 2022 Anthony Green
      - Copyright (c) 1996-2003, 2007, 2008 Red Hat, Inc.
 
@@ -406,7 +406,7 @@ typedef struct {
 
   /* If this is enabled, then a raw closure has the same layout
      as a regular closure.  We use this to install an intermediate
-     handler to do the transaltion, void** -> ffi_raw*.  */
+     handler to do the translation, void** -> ffi_raw*.  */
 
   void     (*translate_args)(ffi_cif*,void*,void**,void*);
   void      *this_closure;
