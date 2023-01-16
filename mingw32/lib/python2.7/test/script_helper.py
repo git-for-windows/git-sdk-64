860e67c83ce1d027646ee2c38fe835ce3b6fb654
Johannes Schindelin <johannes.schindelin@gmx.de>
2023-01-16 10:31:46 +0100

    Remove mingw-w64-i686-boost mingw-w64-i686-icu mingw-w64-i686-libidn mingw-w64-i686-python2 mingw-w64-i686-source-highlight
    These packages were forgotten in 0185b0666c0 (Remove scons python2 quilt
    rcs inetutils tftp-hpa ttyrec texinfo texinfo-tex pax mingw-w64-python2
    mingw-w64-pixman mingw-w64-cairo mingw-w64-libtiff
    mingw-w64-libjpeg-turbo mingw-w64-libpng mingw-w64-libidn mingw-w64-lerc
    mingw-w64-jbigkit mingw-w64-icu mingw-w64-harfbuzz mingw-w64-graphite2
    mingw-w64-giflib mingw-w64-freeglut mingw-w64-fontconfig libidn libdb
    lemon intltool gperf cocom db mingw-w64-boost mingw-w64-freetype
    mingw-w64-gdk-pixbuf2 mingw-w64-jasper mingw-w64-libwebp
    mingw-w64-libgsf mingw-w64-source-highlight, 2022-02-14)
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- mingw32/lib/python2.7/test/script_helper.py
+++
@@ -1,1 +0,0 @@
-from test.support.script_helper import *
--- mingw32/share/doc/source-highlight/api/structsrchilite_1_1ToLower.js
+++
@@ -1,4 +0,0 @@
-var structsrchilite_1_1ToLower =
-[
-    [ "operator()", "structsrchilite_1_1ToLower.html#a3ddcdbcafd9039b30ff6031ccf043311", null ]
-];
