4c0bdc242eaba9d6b6449048d9316ba9fea8bb48
Git for Windows Build Agent <ci@git-for-windows.build>
2023-02-12 03:11:55 +0000

    Update 24 packages
    bash-completion (2.11-2 -> 2.11-3)
    gnutls (3.7.8-2 -> 3.8.0-1)
    isl (0.25-2 -> 0.25-3)
    libgnutls (3.7.8-2 -> 3.8.0-1)
    lndir (1.0.3-2 -> 1.0.4-1)
    mingw-w64-i686-crt-git (10.0.0.r216.gca58fc56b-1 -> 10.0.0.r221.g8708b7d92-1)
    mingw-w64-i686-headers-git (10.0.0.r216.gca58fc56b-1 -> 10.0.0.r221.g8708b7d92-1)
    mingw-w64-i686-libiconv (1.17-1 -> 1.17-2)
    mingw-w64-i686-libmangle-git (10.0.0.r216.gca58fc56b-1 -> 10.0.0.r221.g8708b7d92-1)
    mingw-w64-i686-libwinpthread-git (10.0.0.r216.gca58fc56b-1 -> 10.0.0.r221.g8708b7d92-1)
    mingw-w64-i686-tools-git (10.0.0.r216.gca58fc56b-1 -> 10.0.0.r221.g8708b7d92-1)
    mingw-w64-i686-winpthreads-git (10.0.0.r216.gca58fc56b-1 -> 10.0.0.r221.g8708b7d92-1)
    mingw-w64-i686-zstd (1.5.2-2 -> 1.5.4-1)
    mingw-w64-x86_64-crt-git (10.0.0.r216.gca58fc56b-1 -> 10.0.0.r221.g8708b7d92-1)
    mingw-w64-x86_64-headers-git (10.0.0.r216.gca58fc56b-1 -> 10.0.0.r221.g8708b7d92-1)
    mingw-w64-x86_64-libiconv (1.17-1 -> 1.17-2)
    mingw-w64-x86_64-libmangle-git (10.0.0.r216.gca58fc56b-1 -> 10.0.0.r221.g8708b7d92-1)
    mingw-w64-x86_64-libwinpthread-git (10.0.0.r216.gca58fc56b-1 -> 10.0.0.r221.g8708b7d92-1)
    mingw-w64-x86_64-tools-git (10.0.0.r216.gca58fc56b-1 -> 10.0.0.r221.g8708b7d92-1)
    mingw-w64-x86_64-winpthreads-git (10.0.0.r216.gca58fc56b-1 -> 10.0.0.r221.g8708b7d92-1)
    mingw-w64-x86_64-zstd (1.5.2-2 -> 1.5.4-1)
    perl-Authen-SASL (2.16-2 -> 2.16-3)
    perl-Convert-BinHex (1.125-1 -> 1.125-2)
    swig (4.0.2-2 -> 4.0.2-3)
    
    Signed-off-by: Git for Windows Build Agent <ci@git-for-windows.build>

--- mingw64/lib/cmake/zstd/zstdConfigVersion.cmake
+++ mingw64/lib/cmake/zstd/zstdConfigVersion.cmake
@@ -9,19 +9,19 @@
 # The variable CVF_VERSION must be set before calling configure_file().
 
 
-set(PACKAGE_VERSION "1.5.2")
+set(PACKAGE_VERSION "1.5.4")
 
 if(PACKAGE_VERSION VERSION_LESS PACKAGE_FIND_VERSION)
   set(PACKAGE_VERSION_COMPATIBLE FALSE)
 else()
 
-  if("1.5.2" MATCHES "^([0-9]+)\\.")
+  if("1.5.4" MATCHES "^([0-9]+)\\.")
     set(CVF_VERSION_MAJOR "${CMAKE_MATCH_1}")
     if(NOT CVF_VERSION_MAJOR VERSION_EQUAL 0)
       string(REGEX REPLACE "^0+" "" CVF_VERSION_MAJOR "${CVF_VERSION_MAJOR}")
     endif()
   else()
-    set(CVF_VERSION_MAJOR "1.5.2")
+    set(CVF_VERSION_MAJOR "1.5.4")
   endif()
 
   if(PACKAGE_FIND_VERSION_RANGE)
