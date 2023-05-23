d034c5bae3304a13ff9f47c539993e22b05bf728
Git for Windows Build Agent <ci@git-for-windows.build>
2023-05-23 03:01:12 +0000

    Update 5 packages
    apr (1.7.2-1 -> 1.7.4-1)
    mingw-w64-i686-c-ares (1.19.0-1 -> 1.19.1-1)
    mingw-w64-i686-wineditline (2.205-3 -> 2.206-1)
    mingw-w64-x86_64-c-ares (1.19.0-1 -> 1.19.1-1)
    mingw-w64-x86_64-wineditline (2.205-3 -> 2.206-1)
    
    Signed-off-by: Git for Windows Build Agent <ci@git-for-windows.build>

--- mingw64/include/editline/readline.h
+++ mingw64/include/editline/readline.h
@@ -5,7 +5,7 @@ readline.h
 is part of:
 
 WinEditLine (formerly MinGWEditLine)
-Copyright 2010-2016 Paolo Tosco <paolo.tosco.mail@gmail.com>
+Copyright 2010-2020 Paolo Tosco <paolo.tosco.mail@gmail.com>
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
