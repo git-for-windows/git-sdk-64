ff4d4cf2bbdb6253cfc697274fa15cf36fa70916
Git for Windows Build Agent <ci@git-for-windows.build>
2023-08-06 03:00:04 +0000

    Update 2 packages
    mingw-w64-i686-pkgconf (1~1.9.5-1 -> 1~2.0.0-1)
    mingw-w64-x86_64-pkgconf (1~1.9.5-1 -> 1~2.0.0-1)
    
    Signed-off-by: Git for Windows Build Agent <ci@git-for-windows.build>

--- mingw32/include/pkgconf/libpkgconf/libpkgconf.h
+++ mingw32/include/pkgconf/libpkgconf/libpkgconf.h
@@ -79,8 +79,8 @@ typedef struct pkgconf_cross_personality_ pkgconf_cross_personality_t;
 #define PKGCONF_FOREACH_LIST_ENTRY_REVERSE(tail, value) \
 	for ((value) = (tail); (value) != NULL; (value) = (value)->prev)
 
-#define LIBPKGCONF_VERSION	10905
-#define LIBPKGCONF_VERSION_STR	"1.9.5"
+#define LIBPKGCONF_VERSION	20000
+#define LIBPKGCONF_VERSION_STR	"2.0.0"
 
 struct pkgconf_fragment_ {
 	pkgconf_node_t iter;
@@ -167,8 +167,7 @@ struct pkgconf_pkg_ {
 	pkgconf_tuple_t *prefix;
 
 	uint64_t serial;
-
-	size_t hits;
+	uint64_t identifier;
 };
 
 typedef bool (*pkgconf_pkg_iteration_func_t)(const pkgconf_pkg_t *pkg, void *data);
@@ -204,6 +203,7 @@ struct pkgconf_client_ {
 	bool already_sent_notice;
 
 	uint64_t serial;
+	uint64_t identifier;
 
 	pkgconf_pkg_t **cache_table;
 	size_t cache_count;
