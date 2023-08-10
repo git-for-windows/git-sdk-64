a7c3a4e79d054b2a7b097ec5f5523859e679435b
Git for Windows Build Agent <ci@git-for-windows.build>
2023-08-10 03:02:09 +0000

    Update 2 packages
    mingw-w64-i686-python (3.11.4-4 -> 3.11.4-5)
    mingw-w64-x86_64-python (3.11.4-4 -> 3.11.4-5)
    
    Signed-off-by: Git for Windows Build Agent <ci@git-for-windows.build>

--- mingw32/lib/python3.11/test/__main__.py
+++ mingw32/lib/python3.11/test/__main__.py
@@ -1,2 +1,10 @@
+import os
+import sys
+
 from test.libregrtest import main
+
+if sys.platform == "win32":
+    # Enable DLL loading from PATH.
+    os.environ["PYTHONLEGACYWINDOWSDLLLOADING"] = "1"
+
 main()
