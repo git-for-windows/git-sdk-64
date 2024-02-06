ad5fbeba4bf8078d4c5b34255feafc10dda2b490
Git for Windows Build Agent <ci@git-for-windows.build>
2024-02-06 03:00:00 +0000

    Update 11 packages
    libcbor (0.10.2-1 -> 0.11.0-1)
    libfido2 (1.14.0-1 -> 1.14.0-2)
    libxml2 (2.12.4-1 -> 2.12.5-1)
    mingw-w64-i686-gcc (13.2.0-3 -> 13.2.0-4)
    mingw-w64-i686-gcc-fortran (13.2.0-3 -> 13.2.0-4)
    mingw-w64-i686-gcc-libgfortran (13.2.0-3 -> 13.2.0-4)
    mingw-w64-i686-gcc-libs (13.2.0-3 -> 13.2.0-4)
    mingw-w64-i686-libxml2 (2.12.4-1 -> 2.12.5-1)
    mingw-w64-x86_64-gcc (13.2.0-3 -> 13.2.0-4)
    mingw-w64-x86_64-gcc-libs (13.2.0-3 -> 13.2.0-4)
    mingw-w64-x86_64-libxml2 (2.12.4-1 -> 2.12.5-1)
    
    Signed-off-by: Git for Windows Build Agent <ci@git-for-windows.build>

---
+++ mingw32/lib/gcc/i686-w64-mingw32/13.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-13.2.0/configure --prefix=/mingw32 --with-local-prefix=/mingw32/local --build=i686-w64-mingw32 --host=i686-w64-mingw32 --target=i686-w64-mingw32 --with-native-system-header-dir=/mingw32/include --libexecdir=/mingw32/lib --enable-bootstrap --enable-checking=release --with-arch=pentium4 --with-tune=generic --enable-languages=c,lto,c++,fortran,ada,objc,obj-c++,jit --enable-shared --enable-static --enable-libatomic --enable-threads=posix --enable-graphite --enable-fully-dynamic-string --enable-libstdcxx-filesystem-ts --enable-libstdcxx-time --disable-libstdcxx-pch --enable-lto --enable-libgomp --disable-libssp --disable-multilib --disable-rpath --disable-win32-registry --disable-nls --disable-werror --disable-symvers --with-libiconv --with-system-zlib --with-gmp=/mingw32 --with-mpfr=/mingw32 --with-mpc=/mingw32 --with-isl=/mingw32 --with-pkgversion='Rev4, Built by MSYS2 project' --with-bugurl=https://github.com/msys2/MINGW-packages/issues --with-gnu-as --with-gnu-ld --disable-libstdcxx-debug --disable-sjlj-exceptions --with-dwarf2 --with-boot-ldflags=-static-libstdc++ --with-stage1-ldflags=-static-libstdc++";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "pentium4" }, { "tune", "generic" } };
