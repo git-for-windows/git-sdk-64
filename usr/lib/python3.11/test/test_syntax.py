863c8710628dd990b8ef467df60b602d3e8d7859
Git for Windows Build Agent <ci@git-for-windows.build>
2024-02-14 03:00:02 +0000

    Update 3 packages
    mingw-w64-i686-python (3.11.7-2 -> 3.11.8-1)
    mingw-w64-x86_64-python (3.11.7-2 -> 3.11.8-1)
    python (3.11.7-1 -> 3.11.8-1)
    
    Signed-off-by: Git for Windows Build Agent <ci@git-for-windows.build>

--- usr/lib/python3.11/test/test_syntax.py
+++ usr/lib/python3.11/test/test_syntax.py
@@ -2143,9 +2143,11 @@ func(
 """
         self._check_error(code, "parenthesis '\\)' does not match opening parenthesis '\\['")
 
+        self._check_error("match y:\n case e(e=v,v,", " was never closed")
+
         # Examples with dencodings
         s = b'# coding=latin\n(aaaaaaaaaaaaaaaaa\naaaaaaaaaaa\xb5'
-        self._check_error(s, "'\(' was never closed")
+        self._check_error(s, r"'\(' was never closed")
 
     def test_error_string_literal(self):
 
