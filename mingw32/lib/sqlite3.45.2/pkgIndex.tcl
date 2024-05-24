5cf7e007df70d7c2fd0b5373175580f2e0590fc7
Git for Windows Build Agent <ci@git-for-windows.build>
2024-05-24 03:00:12 +0000

    Update 2 packages
    mingw-w64-i686-sqlite3 (3.45.2-1 -> 3.45.2-2)
    mingw-w64-x86_64-sqlite3 (3.45.2-1 -> 3.45.2-2)
    
    Signed-off-by: Git for Windows Build Agent <ci@git-for-windows.build>

--- mingw32/lib/sqlite3.45.2/pkgIndex.tcl
+++ mingw32/lib/sqlite3.45.2/pkgIndex.tcl
@@ -1,1 +1,1 @@
-package ifneeded sqlite3 3.45.2 [list load [file join $dir libtclsqlite3[info sharedlibextension]] sqlite3]
+package ifneeded sqlite3 3.45.3 [list load [file join $dir libtclsqlite3[info sharedlibextension]] sqlite3]
