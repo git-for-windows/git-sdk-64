454c20845d0469ebfded51088c0ab02a3e0fc71b
Git for Windows Build Agent <ci@git-for-windows.build>
2024-11-22 03:22:18 +0000

    Update 7 packages
    mingw-w64-i686-asciidoctor (2.0.23-1 -> 2.0.23-2)
    mingw-w64-i686-ruby (removed)
    mingw-w64-i686-ruby (new: 3.3.6-1)
    mingw-w64-x86_64-asciidoctor (2.0.23-1 -> 2.0.23-2)
    mingw-w64-x86_64-git-lfs (3.5.1-1 -> 3.6.0-1)
    mingw-w64-x86_64-ruby (removed)
    mingw-w64-x86_64-ruby (new: 3.3.6-1)
    
    Signed-off-by: Git for Windows Build Agent <ci@git-for-windows.build>

---
+++ mingw32/lib/ruby/3.3.0/i386-mingw32/rbconfig.rb
@@ -0,0 +1,364 @@
+# encoding: ascii-8bit
+# frozen-string-literal: false
+#
+# The module storing Ruby interpreter configurations on building.
+#
+# This file was created by mkconfig.rb when ruby was built.  It contains
+# build information for ruby which is used e.g. by mkmf to build
+# compatible native extensions.  Any changes made to this file will be
+# lost the next time ruby is built.
+
+module RbConfig
+  RUBY_VERSION.start_with?("3.3.") or
+    raise "ruby lib version (3.3.6) doesn't match executable version (#{RUBY_VERSION})"
+
+  # Ruby installed directory.
+  TOPDIR = File.dirname(__FILE__).chomp!("/lib/ruby/3.3.0/i386-mingw32")
+  # DESTDIR on make install.
+  DESTDIR = TOPDIR && TOPDIR[/\A[a-z]:/i] || '' unless defined? DESTDIR
+  # The hash configurations stored.
+  CONFIG = {}
+  CONFIG["DESTDIR"] = DESTDIR
+  CONFIG["MAJOR"] = "3"
+  CONFIG["MINOR"] = "3"
+  CONFIG["TEENY"] = "6"
+  CONFIG["PATCHLEVEL"] = "108"
+  CONFIG["INSTALL"] = '/usr/bin/install -c'
+  CONFIG["EXEEXT"] = ".exe"
+  CONFIG["prefix"] = (TOPDIR || DESTDIR + "/mingw32")
+  CONFIG["ruby_install_name"] = "$(RUBY_BASE_NAME)"
+  CONFIG["RUBY_INSTALL_NAME"] = "$(RUBY_BASE_NAME)"
+  CONFIG["RUBY_SO_NAME"] = "msvcrt-$(RUBY_BASE_NAME)$(MAJOR)$(MINOR)0"
+  CONFIG["exec"] = "exec"
+  CONFIG["ruby_pc"] = "ruby-3.3.pc"
+  CONFIG["CC_WRAPPER"] = ""
+  CONFIG["PACKAGE"] = "ruby"
+  CONFIG["BUILTIN_TRANSSRCS"] = " enc/trans/newline.c"
+  CONFIG["MKMF_VERBOSE"] = "0"
+  CONFIG["MANTYPE"] = "man"
+  CONFIG["vendorarchhdrdir"] = "$(vendorhdrdir)/$(sitearch)"
+  CONFIG["sitearchhdrdir"] = "$(sitehdrdir)/$(sitearch)"
+  CONFIG["rubyarchhdrdir"] = "$(rubyhdrdir)/$(arch)"
+  CONFIG["vendorhdrdir"] = "$(rubyhdrdir)/vendor_ruby"
+  CONFIG["sitehdrdir"] = "$(rubyhdrdir)/site_ruby"
+  CONFIG["rubyhdrdir"] = "$(includedir)/$(RUBY_VERSION_NAME)"
+  CONFIG["RUBY_SEARCH_PATH"] = ""
+  CONFIG["UNIVERSAL_INTS"] = ""
+  CONFIG["UNIVERSAL_ARCHNAMES"] = ""
+  CONFIG["configure_args"] = " '--prefix=/mingw32' '--build=i686-w64-mingw32' '--disable-install-doc' '--with-setjmp-type=setjmp' 'build_alias=i686-w64-mingw32' 'CC=gcc' 'CXX=g++' 'CFLAGS=-march=pentium4 -mtune=generic -O2 -pipe -Wp,-D_FORTIFY_SOURCE=2 -fstack-protector-strong -Wp,-D__USE_MINGW_ANSI_STDIO=1 -Wno-incompatible-pointer-types' 'LDFLAGS=-Wl,--no-seh -Wl,--large-address-aware' 'CPPFLAGS=' 'CXXFLAGS=-march=pentium4 -mtune=generic -O2 -pipe -Wp,-D_FORTIFY_SOURCE=2 -fstack-protector-strong -Wp,-D__USE_MINGW_ANSI_STDIO=1'"
+  CONFIG["CONFIGURE"] = "configure"
+  CONFIG["vendorarchdir"] = "$(vendorlibdir)/$(sitearch)"
+  CONFIG["vendorlibdir"] = "$(vendordir)/$(ruby_version)"
+  CONFIG["vendordir"] = "$(rubylibprefix)/vendor_ruby"
+  CONFIG["sitearchdir"] = "$(sitelibdir)/$(sitearch)"
+  CONFIG["sitelibdir"] = "$(sitedir)/$(ruby_version)"
+  CONFIG["sitedir"] = "$(rubylibprefix)/site_ruby"
+  CONFIG["rubyarchdir"] = "$(rubylibdir)/$(arch)"
+  CONFIG["rubylibdir"] = "$(rubylibprefix)/$(ruby_version)"
+  CONFIG["ruby_version"] = "3.3.0"
+  CONFIG["sitearch"] = "i386-msvcrt"
+  CONFIG["arch"] = "i386-mingw32"
+  CONFIG["sitearchincludedir"] = "$(includedir)/$(sitearch)"
+  CONFIG["archincludedir"] = "$(includedir)/$(arch)"
+  CONFIG["sitearchlibdir"] = "$(libdir)/$(sitearch)"
+  CONFIG["archlibdir"] = "$(libdir)/$(arch)"
+  CONFIG["libdirname"] = "libdir"
+  CONFIG["RUBY_EXEC_PREFIX"] = ""
+  CONFIG["RUBY_LIB_VERSION"] = ""
+  CONFIG["RUBY_LIB_VERSION_STYLE"] = "3\t/* full */"
+  CONFIG["RI_BASE_NAME"] = "ri"
+  CONFIG["ridir"] = "$(datarootdir)/$(RI_BASE_NAME)"
+  CONFIG["rubysitearchprefix"] = "$(rubylibprefix)/$(sitearch)"
+  CONFIG["rubyarchprefix"] = "$(rubylibprefix)/$(arch)"
+  CONFIG["MAKEFILES"] = "Makefile GNUmakefile"
+  CONFIG["USE_LLVM_WINDRES"] = "no"
+  CONFIG["PLATFORM_DIR"] = "win32"
+  CONFIG["COROUTINE_TYPE"] = "win32"
+  CONFIG["THREAD_MODEL"] = "win32"
+  CONFIG["SYMBOL_PREFIX"] = "_"
+  CONFIG["EXPORT_PREFIX"] = " "
+  CONFIG["COMMON_HEADERS"] = "winsock2.h windows.h"
+  CONFIG["COMMON_MACROS"] = ""
+  CONFIG["COMMON_LIBS"] = "m"
+  CONFIG["MAINLIBS"] = "-lgmp -lshell32 -lws2_32 -liphlpapi -limagehlp -lshlwapi -lbcrypt "
+  CONFIG["ENABLE_SHARED"] = "yes"
+  CONFIG["DLDSHARED"] = "$(CC) -shared"
+  CONFIG["DLDLIBS"] = ""
+  CONFIG["SOLIBS"] = "$(MAINLIBS)"
+  CONFIG["LIBRUBYARG_SHARED"] = "-l$(RUBY_SO_NAME)"
+  CONFIG["LIBRUBYARG_STATIC"] = "-l$(RUBY_SO_NAME)-static $(MAINLIBS)"
+  CONFIG["LIBRUBYARG"] = "$(LIBRUBYARG_SHARED)"
+  CONFIG["LIBRUBY"] = "lib$(RUBY_SO_NAME).dll.a"
+  CONFIG["LIBRUBY_ALIASES"] = ""
+  CONFIG["LIBRUBY_SONAME"] = ""
+  CONFIG["LIBRUBY_SO"] = "$(RUBY_SO_NAME).dll"
+  CONFIG["LIBRUBY_A"] = "lib$(RUBY_SO_NAME)-static.a"
+  CONFIG["RUBYW_INSTALL_NAME"] = "$(RUBYW_BASE_NAME)"
+  CONFIG["rubyw_install_name"] = "$(RUBYW_INSTALL_NAME)"
+  CONFIG["EXTDLDFLAGS"] = "$(DEFFILE)"
+  CONFIG["EXTLDFLAGS"] = "-static-libgcc"
+  CONFIG["strict_warnflags"] = ""
+  CONFIG["warnflags"] = "-Wall -Wextra -Wdeprecated-declarations -Wdiv-by-zero -Wduplicated-cond -Wimplicit-function-declaration -Wimplicit-int -Wpointer-arith -Wwrite-strings -Wold-style-definition -Wimplicit-fallthrough=0 -Wmissing-noreturn -Wno-cast-function-type -Wno-constant-logical-operand -Wno-long-long -Wno-missing-field-initializers -Wno-overlength-strings -Wno-packed-bitfield-compat -Wno-parentheses-equality -Wno-self-assign -Wno-tautological-compare -Wno-unused-parameter -Wno-unused-value -Wsuggest-attribute=format -Wsuggest-attribute=noreturn -Wunused-variable -Wmisleading-indentation -Wundef"
+  CONFIG["debugflags"] = "-ggdb3"
+  CONFIG["optflags"] = "-O3 -fno-omit-frame-pointer -fno-fast-math"
+  CONFIG["NULLCMD"] = ":"
+  CONFIG["ENABLE_DEBUG_ENV"] = ""
+  CONFIG["DLNOBJ"] = "dln.o"
+  CONFIG["RJIT_SUPPORT"] = "no"
+  CONFIG["YJIT_OBJ"] = ""
+  CONFIG["YJIT_LIBS"] = ""
+  CONFIG["CARGO_BUILD_ARGS"] = ""
+  CONFIG["YJIT_SUPPORT"] = "no"
+  CONFIG["CARGO"] = ""
+  CONFIG["RUSTC"] = "no"
+  CONFIG["INSTALL_STATIC_LIBRARY"] = "no"
+  CONFIG["EXECUTABLE_EXTS"] = ".exe .com .cmd .bat"
+  CONFIG["ARCHFILE"] = ""
+  CONFIG["LIBRUBY_RELATIVE"] = "yes"
+  CONFIG["EXTOUT"] = ".ext"
+  CONFIG["PREP"] = "miniruby$(EXEEXT)"
+  CONFIG["CROSS_COMPILING"] = "no"
+  CONFIG["TEST_RUNNABLE"] = "yes"
+  CONFIG["rubylibprefix"] = "$(libdir)/$(RUBY_BASE_NAME)"
+  CONFIG["setup"] = "Setup"
+  CONFIG["SOEXT"] = "dll"
+  CONFIG["TRY_LINK"] = ""
+  CONFIG["PRELOADENV"] = ""
+  CONFIG["LIBPATHENV"] = "PATH"
+  CONFIG["RPATHFLAG"] = ""
+  CONFIG["LIBPATHFLAG"] = " -L%s"
+  CONFIG["LINK_SO"] = ""
+  CONFIG["ADDITIONAL_DLDFLAGS"] = ""
+  CONFIG["ENCSTATIC"] = ""
+  CONFIG["EXTSTATIC"] = ""
+  CONFIG["ASMEXT"] = "S"
+  CONFIG["LIBEXT"] = "a"
+  CONFIG["DLEXT"] = "so"
+  CONFIG["LDSHAREDXX"] = "$(CXX) -shared"
+  CONFIG["LDSHARED"] = "$(CC) -shared"
+  CONFIG["CCDLFLAGS"] = ""
+  CONFIG["STATIC"] = ""
+  CONFIG["ARCH_FLAG"] = ""
+  CONFIG["DLDFLAGS"] = "-Wl,--no-seh -Wl,--large-address-aware -Wl,--enable-auto-image-base,--enable-auto-import"
+  CONFIG["ALLOCA"] = ""
+  CONFIG["EGREP"] = "/usr/bin/grep -E"
+  CONFIG["GREP"] = "/usr/bin/grep"
+  CONFIG["dsymutil"] = ""
+  CONFIG["codesign"] = ""
+  CONFIG["cleanlibs"] = ""
+  CONFIG["POSTLINK"] = ":"
+  CONFIG["incflags"] = ""
+  CONFIG["WERRORFLAG"] = "-Werror"
+  CONFIG["RUBY_DEVEL"] = ""
+  CONFIG["CHDIR"] = "cd"
+  CONFIG["RMALL"] = "rm -fr"
+  CONFIG["RMDIRS"] = "rmdir --ignore-fail-on-non-empty -p"
+  CONFIG["RMDIR"] = "rmdir --ignore-fail-on-non-empty"
+  CONFIG["CP"] = "cp"
+  CONFIG["RM"] = "rm -f"
+  CONFIG["PKG_CONFIG"] = "pkg-config"
+  CONFIG["DOXYGEN"] = ""
+  CONFIG["DOT"] = ""
+  CONFIG["MKDIR_P"] = "/usr/bin/mkdir -p"
+  CONFIG["INSTALL_DATA"] = "$(INSTALL) -m 644"
+  CONFIG["INSTALL_SCRIPT"] = "$(INSTALL)"
+  CONFIG["INSTALL_PROGRAM"] = "$(INSTALL)"
+  CONFIG["SET_MAKE"] = ""
+  CONFIG["LN_S"] = "cp -pR"
+  CONFIG["DLLWRAP"] = "dllwrap"
+  CONFIG["WINDRES"] = "windres"
+  CONFIG["ASFLAGS"] = ""
+  CONFIG["ARFLAGS"] = "rcD "
+  CONFIG["try_header"] = ""
+  CONFIG["CC_VERSION_MESSAGE"] = "gcc.exe (Rev2, Built by MSYS2 project) 14.2.0\nCopyright (C) 2024 Free Software Foundation, Inc.\nThis is free software; see the source for copying conditions.  There is NO\nwarranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE."
+  CONFIG["CC_VERSION"] = "$(CC) --version"
+  CONFIG["CSRCFLAG"] = ""
+  CONFIG["COUTFLAG"] = "-o "
+  CONFIG["OUTFLAG"] = "-o "
+  CONFIG["CPPOUTFILE"] = "-o conftest.i"
+  CONFIG["GNU_LD"] = "yes"
+  CONFIG["GCC"] = "yes"
+  CONFIG["CPP"] = "$(CC) -E"
+  CONFIG["CXXFLAGS"] = "-march=pentium4 -mtune=generic -O2 -pipe -Wp,-D_FORTIFY_SOURCE=2 -fstack-protector-strong -Wp,-D__USE_MINGW_ANSI_STDIO=1"
+  CONFIG["OBJEXT"] = "o"
+  CONFIG["CPPFLAGS"] = "-D_WIN32_WINNT=0x0600 -D__MINGW_USE_VC2005_COMPAT $(DEFS) $(cppflags)"
+  CONFIG["LDFLAGS"] = "-L. -Wl,--no-seh -Wl,--large-address-aware -fstack-protector-strong -Wl,--no-as-needed"
+  CONFIG["CFLAGS"] = "-march=pentium4 -mtune=generic -O2 -pipe -Wp,-D_FORTIFY_SOURCE=2 -fstack-protector-strong -Wp,-D__USE_MINGW_ANSI_STDIO=1 -Wno-incompatible-pointer-types"
+  CONFIG["STRIP"] = "strip -S -x"
+  CONFIG["RANLIB"] = "gcc-ranlib"
+  CONFIG["OBJDUMP"] = "objdump"
+  CONFIG["OBJCOPY"] = ":"
+  CONFIG["NM"] = "gcc-nm"
+  CONFIG["LD"] = "ld"
+  CONFIG["CXX"] = "g++"
+  CONFIG["AS"] = "as"
+  CONFIG["AR"] = "gcc-ar"
+  CONFIG["CC"] = "gcc"
+  CONFIG["wasmoptflags"] = ""
+  CONFIG["WASMOPT"] = ""
+  CONFIG["target_os"] = "mingw32"
+  CONFIG["target_vendor"] = "w64"
+  CONFIG["target_cpu"] = "i386"
+  CONFIG["target"] = "$(target_cpu)-$(target_vendor)-$(target_os)"
+  CONFIG["host_os"] = "$(target_os)"
+  CONFIG["host_vendor"] = "$(target_vendor)"
+  CONFIG["host_cpu"] = "$(target_cpu)"
+  CONFIG["host"] = "$(target)"
+  CONFIG["build_os"] = "mingw32"
+  CONFIG["build_vendor"] = "w64"
+  CONFIG["build_cpu"] = "i686"
+  CONFIG["build"] = "i686-w64-mingw32"
+  CONFIG["RUBY_VERSION_NAME"] = "$(RUBY_BASE_NAME)-$(ruby_version)"
+  CONFIG["RUBYW_BASE_NAME"] = "rubyw"
+  CONFIG["RUBY_BASE_NAME"] = "ruby"
+  CONFIG["RUBY_PROGRAM_VERSION"] = "$(MAJOR).$(MINOR).$(TEENY)"
+  CONFIG["RUBY_API_VERSION"] = "$(MAJOR).$(MINOR)"
+  CONFIG["HAVE_GIT"] = "no"
+  CONFIG["GIT"] = "git"
+  CONFIG["cxxflags"] = ""
+  CONFIG["cppflags"] = ""
+  CONFIG["cflags"] = "$(optflags) $(debugflags) $(warnflags)"
+  CONFIG["MAKEDIRS"] = "/usr/bin/mkdir -p"
+  CONFIG["target_alias"] = ""
+  CONFIG["host_alias"] = "$(target_alias)"
+  CONFIG["build_alias"] = "i686-w64-mingw32"
+  CONFIG["LIBS"] = "-lshell32 -lws2_32 -liphlpapi -limagehlp -lshlwapi -lbcrypt "
+  CONFIG["ECHO_T"] = ""
+  CONFIG["ECHO_N"] = "-n"
+  CONFIG["ECHO_C"] = ""
+  CONFIG["DEFS"] = "-D_FILE_OFFSET_BITS=64"
+  CONFIG["mandir"] = "$(datarootdir)/man"
+  CONFIG["localedir"] = "$(datarootdir)/locale"
+  CONFIG["libdir"] = "$(exec_prefix)/lib"
+  CONFIG["psdir"] = "$(docdir)"
+  CONFIG["pdfdir"] = "$(docdir)"
+  CONFIG["dvidir"] = "$(docdir)"
+  CONFIG["htmldir"] = "$(docdir)"
+  CONFIG["infodir"] = "$(datarootdir)/info"
+  CONFIG["docdir"] = "$(datarootdir)/doc/$(PACKAGE)"
+  CONFIG["oldincludedir"] = "/usr/include"
+  CONFIG["includedir"] = "$(prefix)/include"
+  CONFIG["runstatedir"] = "$(localstatedir)/run"
+  CONFIG["localstatedir"] = "$(prefix)/var"
+  CONFIG["sharedstatedir"] = "$(prefix)/com"
+  CONFIG["sysconfdir"] = ""
+  CONFIG["datadir"] = "$(datarootdir)"
+  CONFIG["datarootdir"] = "$(prefix)/share"
+  CONFIG["libexecdir"] = "$(exec_prefix)/libexec"
+  CONFIG["sbindir"] = "$(exec_prefix)/sbin"
+  CONFIG["bindir"] = "$(exec_prefix)/bin"
+  CONFIG["exec_prefix"] = "$(prefix)"
+  CONFIG["PACKAGE_URL"] = ""
+  CONFIG["PACKAGE_BUGREPORT"] = ""
+  CONFIG["PACKAGE_STRING"] = ""
+  CONFIG["PACKAGE_VERSION"] = ""
+  CONFIG["PACKAGE_TARNAME"] = ""
+  CONFIG["PACKAGE_NAME"] = ""
+  CONFIG["PATH_SEPARATOR"] = ":"
+  CONFIG["SHELL"] = "/bin/sh"
+  CONFIG["UNICODE_VERSION"] = "15.0.0"
+  CONFIG["UNICODE_EMOJI_VERSION"] = "15.0"
+  CONFIG["platform"] = "$(arch)"
+  CONFIG["archdir"] = "$(rubyarchdir)"
+  CONFIG["topdir"] = File.dirname(__FILE__)
+  # Almost same with CONFIG. MAKEFILE_CONFIG has other variable
+  # reference like below.
+  #
+  #   MAKEFILE_CONFIG["bindir"] = "$(exec_prefix)/bin"
+  #
+  # The values of this constant is used for creating Makefile.
+  #
+  #   require 'rbconfig'
+  #
+  #   print <<-END_OF_MAKEFILE
+  #   prefix = #{RbConfig::MAKEFILE_CONFIG['prefix']}
+  #   exec_prefix = #{RbConfig::MAKEFILE_CONFIG['exec_prefix']}
+  #   bindir = #{RbConfig::MAKEFILE_CONFIG['bindir']}
+  #   END_OF_MAKEFILE
+  #
+  #   => prefix = /usr/local
+  #      exec_prefix = $(prefix)
+  #      bindir = $(exec_prefix)/bin  MAKEFILE_CONFIG = {}
+  #
+  # RbConfig.expand is used for resolving references like above in rbconfig.
+  #
+  #   require 'rbconfig'
+  #   p RbConfig.expand(RbConfig::MAKEFILE_CONFIG["bindir"])
+  #   # => "/usr/local/bin"
+  MAKEFILE_CONFIG = {}
+  CONFIG.each{|k,v| MAKEFILE_CONFIG[k] = v.dup}
+
+  # call-seq:
+  #
+  #   RbConfig.expand(val)         -> string
+  #   RbConfig.expand(val, config) -> string
+  #
+  # expands variable with given +val+ value.
+  #
+  #   RbConfig.expand("$(bindir)") # => /home/foobar/all-ruby/ruby19x/bin
+  def RbConfig::expand(val, config = CONFIG)
+    newval = val.gsub(/\$\$|\$\(([^()]+)\)|\$\{([^{}]+)\}/) {
+      var = $&
+      if !(v = $1 || $2)
+	'$'
+      elsif key = config[v = v[/\A[^:]+(?=(?::(.*?)=(.*))?\z)/]]
+	pat, sub = $1, $2
+	config[v] = false
+	config[v] = RbConfig::expand(key, config)
+	key = key.gsub(/#{Regexp.quote(pat)}(?=\s|\z)/n) {sub} if pat
+	key
+      else
+	var
+      end
+    }
+    val.replace(newval) unless newval == val
+    val
+  end
+  CONFIG.each_value do |val|
+    RbConfig::expand(val)
+  end
+
+  # call-seq:
+  #
+  #   RbConfig.fire_update!(key, val)               -> array
+  #   RbConfig.fire_update!(key, val, mkconf, conf) -> array
+  #
+  # updates +key+ in +mkconf+ with +val+, and all values depending on
+  # the +key+ in +mkconf+.
+  #
+  #   RbConfig::MAKEFILE_CONFIG.values_at("CC", "LDSHARED") # => ["gcc", "$(CC) -shared"]
+  #   RbConfig::CONFIG.values_at("CC", "LDSHARED")          # => ["gcc", "gcc -shared"]
+  #   RbConfig.fire_update!("CC", "gcc-8")                  # => ["CC", "LDSHARED"]
+  #   RbConfig::MAKEFILE_CONFIG.values_at("CC", "LDSHARED") # => ["gcc-8", "$(CC) -shared"]
+  #   RbConfig::CONFIG.values_at("CC", "LDSHARED")          # => ["gcc-8", "gcc-8 -shared"]
+  #
+  # returns updated keys list, or +nil+ if nothing changed.
+  def RbConfig.fire_update!(key, val, mkconf = MAKEFILE_CONFIG, conf = CONFIG) # :nodoc:
+    return if mkconf[key] == val
+    mkconf[key] = val
+    keys = [key]
+    deps = []
+    begin
+      re = Regexp.new("\\$\\((?:%1$s)\\)|\\$\\{(?:%1$s)\\}" % keys.join('|'))
+      deps |= keys
+      keys.clear
+      mkconf.each {|k,v| keys << k if re =~ v}
+    end until keys.empty?
+    deps.each {|k| conf[k] = mkconf[k].dup}
+    deps.each {|k| expand(conf[k])}
+    deps
+  end
+
+  # call-seq:
+  #
+  #   RbConfig.ruby -> path
+  #
+  # returns the absolute pathname of the ruby command.
+  def RbConfig.ruby
+    File.join(
+      RbConfig::CONFIG["bindir"],
+      RbConfig::CONFIG["ruby_install_name"] + RbConfig::CONFIG["EXEEXT"]
+    )
+  end
+end
+CROSS_COMPILING = nil unless defined? CROSS_COMPILING
---
+++ mingw64/lib/ruby/gems/3.3.0/specifications/default/win32ole-1.8.10.gemspec
@@ -0,0 +1,22 @@
+# -*- encoding: utf-8 -*-
+# stub: win32ole 1.8.10 ruby lib
+
+Gem::Specification.new do |s|
+  s.name = "win32ole".freeze
+  s.version = "1.8.10".freeze
+
+  s.required_rubygems_version = Gem::Requirement.new(">= 0".freeze) if s.respond_to? :required_rubygems_version=
+  s.metadata = { "homepage_uri" => "https://github.com/ruby/win32ole", "source_code_uri" => "https://github.com/ruby/win32ole" } if s.respond_to? :metadata=
+  s.require_paths = ["lib".freeze]
+  s.authors = ["Masaki Suketa".freeze]
+  s.bindir = "exe".freeze
+  s.date = "2024-11-05"
+  s.description = "Provides an interface for OLE Automation in Ruby".freeze
+  s.email = ["suke@ruby-lang.org".freeze]
+  s.files = ["win32ole.rb".freeze, "win32ole.so".freeze, "win32ole/property.rb".freeze]
+  s.homepage = "https://github.com/ruby/win32ole".freeze
+  s.licenses = ["Ruby".freeze, "BSD-2-Clause".freeze]
+  s.required_ruby_version = Gem::Requirement.new(">= 2.3.0".freeze)
+  s.rubygems_version = "3.5.22".freeze
+  s.summary = "Provides an interface for OLE Automation in Ruby".freeze
+end
