e5a0e66aef0ad5e1a28b16ee76a1a884eea2201c
Git for Windows Build Agent <ci@git-for-windows.build>
2025-01-26 00:39:58 +0000

    Update 8 packages
    libgpgme (1.24.1-1 -> 1.24.1-2)
    liblzma (5.6.3-1 -> 5.6.4-1)
    mingw-w64-i686-xz (5.6.3-3 -> 5.6.4-1)
    mingw-w64-x86_64-xz (5.6.3-3 -> 5.6.4-1)
    msys2-runtime (3.5.4-5 -> 3.5.5-3)
    msys2-runtime-devel (3.5.4-5 -> 3.5.5-3)
    python (3.12.8-1 -> 3.12.8-2)
    xz (5.6.3-1 -> 5.6.4-1)
    
    Signed-off-by: Git for Windows Build Agent <ci@git-for-windows.build>

---
+++ mingw64/share/doc/xz/api/bcj_8h.js
@@ -0,0 +1,12 @@
+var bcj_8h =
+[
+    [ "lzma_options_bcj", "structlzma__options__bcj.html", "structlzma__options__bcj" ],
+    [ "LZMA_FILTER_X86", "bcj_8h.html#aa9eac1f580ddde3309518cd153d596b1", null ],
+    [ "LZMA_FILTER_POWERPC", "bcj_8h.html#ab7f667d4a5d319f227f23163cbea086f", null ],
+    [ "LZMA_FILTER_IA64", "bcj_8h.html#a2fe36218a38f400e1ce40820758f7427", null ],
+    [ "LZMA_FILTER_ARM", "bcj_8h.html#a495a58f63ebc7a8b756099efba492f8b", null ],
+    [ "LZMA_FILTER_ARMTHUMB", "bcj_8h.html#a5ec62e7e5e7df3d9af5b2ea3f857689a", null ],
+    [ "LZMA_FILTER_SPARC", "bcj_8h.html#a50941088e93ef659c6b000bbcaf58143", null ],
+    [ "LZMA_FILTER_ARM64", "bcj_8h.html#a01765158cd31cac21b272b180628fc4b", null ],
+    [ "LZMA_FILTER_RISCV", "bcj_8h.html#a932e9d66e945f5601b8fad7445a9b40c", null ]
+];
