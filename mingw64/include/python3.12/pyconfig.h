3dc065306205f615a6c3da20322700227d5582fc
Git for Windows Build Agent <ci@git-for-windows.build>
2025-02-07 03:13:49 +0000

    Update 2 packages
    mingw-w64-i686-python (3.12.8-2 -> 3.12.9-1)
    mingw-w64-x86_64-python (3.12.8-2 -> 3.12.9-1)
    
    Signed-off-by: Git for Windows Build Agent <ci@git-for-windows.build>

--- mingw64/include/python3.12/pyconfig.h
+++ mingw64/include/python3.12/pyconfig.h
@@ -1333,6 +1333,9 @@
 /* Define to 1 if you have the <sys/param.h> header file. */
 #define HAVE_SYS_PARAM_H 1
 
+/* Define to 1 if you have the <sys/pidfd.h> header file. */
+/* #undef HAVE_SYS_PIDFD_H */
+
 /* Define to 1 if you have the <sys/poll.h> header file. */
 /* #undef HAVE_SYS_POLL_H */
 
@@ -1436,8 +1439,8 @@
 /* Define to 1 if you have the `truncate' function. */
 /* #undef HAVE_TRUNCATE */
 
-/* Define to 1 if you have the `ttyname' function. */
-/* #undef HAVE_TTYNAME */
+/* Define to 1 if you have the `ttyname_r' function. */
+/* #undef HAVE_TTYNAME_R */
 
 /* Define to 1 if you don't have `tm_zone' but do have the external array
    `tzname'. */
