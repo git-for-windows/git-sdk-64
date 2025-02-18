60dc0a5f0b1afeabe4171ce3a133e023f6b410b9
Jeremy Drake <github@jdrake.com>
2025-02-17 21:54:04 -0800

    Update 22 packages
    glib2 (removed)
    icu (removed)
    libatomic_ops (removed)
    libgpgme (removed)
    liblzo2 (removed)
    mingw-w64-i686-gnutls (removed)
    mingw-w64-i686-jansson (removed)
    mingw-w64-i686-jemalloc (removed)
    mingw-w64-i686-libxml2 (removed)
    mingw-w64-i686-nettle (removed)
    mingw-w64-i686-pdcurses (removed)
    mingw-w64-i686-rtmpdump (removed)
    mingw-w64-i686-spdylay (removed)
    mingw-w64-x86_64-jansson (removed)
    mingw-w64-x86_64-jemalloc (removed)
    mingw-w64-x86_64-libdeflate (removed)
    mingw-w64-x86_64-libxml2 (removed)
    mingw-w64-x86_64-lzo2 (removed)
    mingw-w64-x86_64-pdcurses (removed)
    mingw-w64-x86_64-rtmpdump (removed)
    mingw-w64-x86_64-spdylay (removed)
    tcl (removed)
    
    Signed-off-by: Jeremy Drake <github@jdrake.com>

--- mingw64/include/pdcurses.h
+++
@@ -1,5 +0,0 @@
-/* if you want to use the DLL one: #define PDC_DLL_BUILD 1 */
-/* if you want to use ncurses compatible mouse: #define PDC_NCMOUSE 1 */
-#define PDC_WIDE 1
-#define PDC_FORCE_UTF8 1
-#include "pdcurses/curses.h"
