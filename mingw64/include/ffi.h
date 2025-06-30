3c7a9f29705894df62363459e7064541dedc4232
Git for Windows Build Agent <ci@git-for-windows.build>
2025-06-30 03:48:13 +0000

    Update 4 packages
    mingw-w64-i686-libffi (3.5.0-1 -> 3.5.1-1)
    mingw-w64-i686-sqlite3 (3.50.1-1 -> 3.50.2-1)
    mingw-w64-x86_64-libffi (3.5.0-1 -> 3.5.1-1)
    mingw-w64-x86_64-sqlite3 (3.50.1-1 -> 3.50.2-1)
    
    Signed-off-by: Git for Windows Build Agent <ci@git-for-windows.build>

--- mingw64/include/ffi.h
+++ mingw64/include/ffi.h
@@ -1,5 +1,5 @@
 /* -----------------------------------------------------------------*-C-*-
-   libffi 3.5.0
+   libffi 3.5.1
      - Copyright (c) 2011, 2014, 2019, 2021, 2022, 2024, 2025 Anthony Green
      - Copyright (c) 1996-2003, 2007, 2008 Red Hat, Inc.
 
@@ -316,8 +316,8 @@ size_t ffi_java_raw_size (ffi_cif *cif) __attribute__((deprecated));
 
 /* ---- Version API ------------------------------------------------------ */
 
-#define FFI_VERSION_STRING "3.5.0"
-#define FFI_VERSION_NUMBER 30500
+#define FFI_VERSION_STRING "3.5.1"
+#define FFI_VERSION_NUMBER 30501
 
 #ifndef LIBFFI_ASM
 /* Return a version string. */
