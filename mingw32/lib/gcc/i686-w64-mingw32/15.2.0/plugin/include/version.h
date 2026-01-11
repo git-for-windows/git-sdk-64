d2ec0438d2b52149624845099ce95d168a58abed
Git for Windows Build Agent <ci@git-for-windows.build>
2026-01-11 04:06:52 +0000

    Update 12 packages
    mingw-w64-i686-gcc (15.2.0-8 -> 15.2.0-9)
    mingw-w64-i686-gcc-fortran (15.2.0-8 -> 15.2.0-9)
    mingw-w64-i686-gcc-libgfortran (15.2.0-8 -> 15.2.0-9)
    mingw-w64-i686-gcc-libs (15.2.0-8 -> 15.2.0-9)
    mingw-w64-i686-gdb (17.1-1 -> 17.1-2)
    mingw-w64-i686-libb2 (new: 0.98.1-3)
    mingw-w64-i686-python (3.12.12-1 -> 3.13.11-4)
    mingw-w64-x86_64-gcc (15.2.0-8 -> 15.2.0-9)
    mingw-w64-x86_64-gcc-libs (15.2.0-8 -> 15.2.0-9)
    mingw-w64-x86_64-gdb (17.1-1 -> 17.1-2)
    mingw-w64-x86_64-libb2 (new: 0.98.1-3)
    mingw-w64-x86_64-python (3.12.12-1 -> 3.13.11-4)
    
    Signed-off-by: Git for Windows Build Agent <ci@git-for-windows.build>

--- mingw32/lib/gcc/i686-w64-mingw32/15.2.0/plugin/include/version.h
+++ mingw32/lib/gcc/i686-w64-mingw32/15.2.0/plugin/include/version.h
@@ -10,7 +10,7 @@ BASEVER, DATESTAMP, DEVPHASE, and REVISION are defined by the
 Makefile.  */
 
 #define version_string "15.2.0"
-#define pkgversion_string "(Rev8, Built by MSYS2 project) "
+#define pkgversion_string "(Rev9, Built by MSYS2 project) "
 
 /* This is the location of the online document giving instructions for
 reporting bugs.  If you distribute a modified version of GCC,
