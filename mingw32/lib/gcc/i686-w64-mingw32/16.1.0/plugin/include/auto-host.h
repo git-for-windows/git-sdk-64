2f76f4634668e9169f5f23a720590b99b1390c59
Git for Windows Build Agent <ci@git-for-windows.build>
2026-05-05 05:33:25 +0000

    Update 15 packages
    binutils (2.46-1 -> 2.46-2)
    libgpg-error (1.59-1 -> 1.60-1)
    libunrar (7.2.5-1 -> 7.2.6-1)
    mingw-w64-i686-gcc (16.1.0-1 -> 16.1.0-2)
    mingw-w64-i686-gcc-libs (16.1.0-1 -> 16.1.0-2)
    mingw-w64-i686-gdb (17.1-3 -> 17.1-4)
    mingw-w64-i686-zstd (1.5.7-1 -> 1.5.7-2)
    mingw-w64-x86_64-gcc (16.1.0-1 -> 16.1.0-2)
    mingw-w64-x86_64-gcc-libs (16.1.0-1 -> 16.1.0-2)
    mingw-w64-x86_64-gdb (17.1-3 -> 17.1-4)
    mingw-w64-x86_64-zstd (1.5.7-1 -> 1.5.7-2)
    popt (new: 1.19-1)
    rsync (3.4.1-1 -> 3.4.2-2)
    unrar (7.2.5-1 -> 7.2.6-1)
    vim (9.2.0390-1 -> 9.2.0437-1)
    
    Signed-off-by: Git for Windows Build Agent <ci@git-for-windows.build>

--- mingw32/lib/gcc/i686-w64-mingw32/16.1.0/plugin/include/auto-host.h
+++ mingw32/lib/gcc/i686-w64-mingw32/16.1.0/plugin/include/auto-host.h
@@ -227,7 +227,7 @@
 
 /* Define to enable LTO support. */
 #ifndef USED_FOR_TARGET
-/* #undef ENABLE_LTO */
+#define ENABLE_LTO 1
 #endif
 
 
@@ -1901,7 +1901,7 @@
 
 /* Define to the level of your linker's plugin support. */
 #ifndef USED_FOR_TARGET
-#define HAVE_LTO_PLUGIN 0
+#define HAVE_LTO_PLUGIN 2
 #endif
 
 
