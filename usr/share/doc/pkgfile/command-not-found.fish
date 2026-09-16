55fd570c065898b5326338264afe889c735dc91c
Git for Windows Build Agent <ci@git-for-windows.build>
2026-09-16 07:57:28 +0000

    Update 1 package
    pkgfile (21-2 -> 26-1)
    
    Signed-off-by: Git for Windows Build Agent <ci@git-for-windows.build>

--- usr/share/doc/pkgfile/command-not-found.fish
+++ usr/share/doc/pkgfile/command-not-found.fish
@@ -1,4 +1,4 @@
-function command_not_found_handler --on-event fish_command_not_found
+function fish_command_not_found
     set cmd $argv
 
     if set pkgs (pkgfile -bv -- "$cmd" 2>/dev/null)
--- usr/share/doc/pkgfile/command-not-found.zsh
+++ usr/share/doc/pkgfile/command-not-found.zsh
@@ -3,6 +3,17 @@ command_not_found_handler() {
 
   pkgs=(${(f)"$(pkgfile -b -v -- "$cmd" 2>/dev/null)"})
   if [[ -n "$pkgs" ]]; then
+    if [[ ${#pkgs[@]} -eq 1 && -n $PKGFILE_PROMPT_INSTALL_MISSING ]]; then
+      local pkg=${pkgs[1]%% *}
+      local response
+
+      read -r "response?Install $pkg? [Y/n] "
+      [[ -z $response || $response = [Yy] ]] || return 0
+      printf '\n'
+      sudo pacman -S --noconfirm -- "$pkg"
+      return
+    fi
+
     printf '%s may be found in the following packages:\n' "$cmd"
     printf '  %s\n' $pkgs[@]
   else
